8da7f79d680898112136a1f8ba56c1fe96097286
feng <yinmathematic@gmail.com>
2025-11-06 18:35:29 +0800

    add test process and save the model

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from torchvision import datasets, transforms
+from ConvNet import ConvNet
 from SimpleNN import SimpleNN
 import config as config
 from train import train
@@ -11,16 +9,25 @@ from test import test
 torch.manual_seed(42)
 
 
-model = SimpleNN().to(config.device)
+# model = SimpleNN().to(config.device)
+model = ConvNet().to(config.device) 
 
 print("开始训练模型...")
 train_losses, train_accuracies = [], []
+test_losses, test_accuracies = [], []
 
 for epoch in range(config.num_epochs):
     print(f'\nEpoch {epoch + 1}/{config.num_epochs}')
     train_loss, train_accuracy = train(model)
     train_losses.append(train_loss)
     train_accuracies.append(train_accuracy)
+
+    test_loss, test_accuracy = test(model, config.device, config.test_loader, config.criterion)
+    test_losses.append(test_loss)
+    test_accuracies.append(test_accuracy)
+
+    torch.save(model.state_dict(), f'model_epoch_{epoch+1}.pth')
+
 
 
 
